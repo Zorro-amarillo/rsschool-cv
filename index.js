a692a84e830292992f77e836bfc2007f79c910a3
Yana Malakhova <ya6ka0@gmail.com>
2024-03-28 01:46:42 +0300

    fix: fix projects styles for desktop

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
-const projectBtns = document.querySelectorAll('.project__button'),
-      projectTitles = document.querySelectorAll('.project__title');
+const projectBtns = document.querySelectorAll('.projects__button'),
+      projectTitles = document.querySelectorAll('.projects__title');
 
 function addButtonBg() {
     projectBtns.forEach((btn, i) => {
         btn.style.background = `url('assets/img/projects/0${i + 1}_project.jpg')`;
+        btn.style.backgroundSize = 'cover';
+        btn.style.backgroundPosition = 'center';
     });
 }
 
@@ -11,12 +13,12 @@ addButtonBg();
 
 projectBtns.forEach((btn, i) => {
     btn.addEventListener('mouseover', () => {
-        projectTitles[i].classList.add('project__title_hover');
+        projectTitles[i].classList.add('projects__title_hover');
         projectTitles[i].style.transition = 'all';
     });
 
     btn.addEventListener('mouseout', () => {
-        projectTitles[i].classList.remove('project__title_hover');
+        projectTitles[i].classList.remove('projects__title_hover');
         projectTitles[i].style.transition = 'all';
     });
 });
